e2becc07a7c968724a3068f90a96dd785b0b3234
Developer Sam <dev_sam17@protonmail.com>
2023-04-02 12:25:40 +0000

    added mongodb and databases for products

--- api/controllers/products.js
+++ api/controllers/products.js
@@ -1,52 +1,99 @@
 const ProductsService = require('../services/products')
+const mongoose = require('mongoose');
+
+const Product = require('../models/products');
 
 const getProducts = (req, res, next) => {
-	res.status(200).json({
-		products: ProductsService.getProducts(),
-	})
+	Product.find().exec().then(docs => {
+		console.log(docs);
+		res.status(200).json(docs);
+	}
+	).catch(err => {
+		console.log(err);
+		res.status(500).json({
+			error: err
+		})
+	});
 }
 
 const createProduct = (req, res, next) => {
-	const product = {
-		id: String(new Date().getTime()),
+	const product = new Product({
+		_id: new mongoose.Types.ObjectId(),
 		name: req.body.name,
 		price: req.body.price,
-	}
-
-	const createdProduct = ProductsService.createProduct(product)
-
-	res.status(200).json({
-		createdProduct,
 	})
+	product.save().then(result => {
+		console.log(result);
+		res.status(200).json({
+			message: "handling POST requests to /products",
+			createdProduct: result,
+		})
+	})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json({
+				error: err,
+			})
+		});
+
+//	const createdProduct = ProductsService.createProduct(product)
+
+	
 }
 
 const getProductById = (req, res, next) => {
 	const id = req.params.productId
-	const product = ProductsService.getProductById(id)
-
-	if (!product) {
-		return res.status(404).json({
-			message: 'Product not found',
-		})
-	}
-	res.status(200).json({
-		product,
-	})
+	Product.findById(id).exec().then(doc => {
+		console.log("from databae", doc);
+		if (doc) {
+			res.status(200).json(doc);
+		} else {
+			res.status(404).json({message: 'No valid entry found for this id'})
+		}
+	}).catch(err => {
+		console.log(err);
+		res.status(500).json({error: err});
+	});
 }
+//	const product = ProductsService.getProductById(id)
+
+// 	if (!product) {
+// 		return res.status(404).json({
+// 			message: 'Product not found',
+// 		})
+// 	}
+// 	res.status(200).json({
+// 		product,
+// 	})
+// }
 
 const updateProductById = (req, res, next) => {
-	res.status(200).json({
-		message: ProductsService.updateProductById(
-			req.params.productId,
-			req.body
-		),
-	})
+	const id = req.params.productId;
+	const updateOps = {};
+	for (const ops of req.body) {
+		updateOps[ops.propName] = ops.value;
+	}
+	Product.findByIdAndUpdate(id, { $set: updateOps}).exec().then(result => {
+		console.log(result);
+		res.status(200).json(result);
+	}).catch(err => {
+		console.log(err);
+		res.status(500).json({
+			error: err
+		})
+	});
 }
 
 const deleteProductById = (req, res, next) => {
-	res.status(200).json({
-		message: ProductsService.deleteProductById(req.params.productId),
-	})
+	const id = req.params.productId;
+	Product.findByIdAndDelete(id).exec().then(result => {
+		res.status(200).json(result);
+	}).catch(err => {
+		console.log(err);
+		res.status(500).json({
+			error: err
+		})
+	});
 }
 
 module.exports = {
--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const productRoutes = require('./api/routes/products')
 const orderRoutes = require('./api/routes/orders')
+
+// mongoose.connect('mongodb+srv://samrock17:' + process.env.MONGO_ATLAS_PW + '@cluster0.poipsye.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://samrock17:samsing17@cluster0.poipsye.mongodb.net/?retryWrites=true&w=majority')
 
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({ extended: false }))
